295806c0c0f6e75ea4e38cc903eeeeeaafdafc0f
Marivi Pelaez Alonso <mariavictoria.pelaezalonso@telefonica.com>
2015-04-23 18:00:07 +0200

    First kNN classifier based on Euclidean distance

--- ch2_knn/kNN.py
+++ ch2_knn/kNN.py
@@ -16,6 +16,7 @@ u"""Copyright 2015 Mariví Peláez
 """
 
 import numpy as np
+import operator
 
 
 def create_dataset():
@@ -25,3 +26,41 @@ def create_dataset():
     group = np.array([[1.0, 1.1], [1.0, 1.0], [0, 0], [0, 0.1]])
     labels = ['A', 'A', 'B', 'B']
     return group, labels
+
+
+def classify(test_vector, training_dataset, training_classes, k):
+    """Classification for kNN algorithm, using Euclidean distance
+
+        d = sqrt((x1 - x2)^2 + (y1 - y2)^2)
+
+    :param test_vector: vector of elements to be classified
+    :param training_dataset: matrix of training elements
+    :param training_classes: sorted vector of classes of the training dataset
+    :param k: number of neighbors to use in the comparison algorithm
+    :return: class of the given test_vector
+    """
+    # shape[0] = number of rows
+    # shape[1] = number of columns
+
+    # Number of elements in the training dataset
+    dataset_size = training_dataset.shape[0]
+
+    # Distance calculation using Euclidean distance
+    diff_mat = np.tile(test_vector, (dataset_size, 1)) - training_dataset
+    sq_diff_mat = diff_mat**2
+    sq_distances = sq_diff_mat.sum(axis=1)
+    distances = sq_distances**0.5
+
+    # Sorting distances
+    sorted_distances_indices = distances.argsort()
+
+    # Getting k lowest distances
+    class_count = {}
+    for i in range(k):
+        vote_class = training_classes[sorted_distances_indices[i]]
+        class_count[vote_class] = class_count.get(vote_class, 0) + 1
+
+    # Sorting output dictionary
+    sorted_class_count = sorted(class_count.iteritems(), key=operator.itemgetter(1), reverse=True)
+
+    return sorted_class_count[0][0]
